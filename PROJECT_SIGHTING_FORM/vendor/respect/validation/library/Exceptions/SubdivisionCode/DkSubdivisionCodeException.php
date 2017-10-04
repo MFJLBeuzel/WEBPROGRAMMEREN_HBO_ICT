41a0860b0f8836162fe060e553747238199d8091
MFJLBeuzel <maartenbeuzel@gmail.com>
2017-10-04 18:47:15 +0200

    ...
    ...

---
+++ PROJECT_SIGHTING_FORM/vendor/respect/validation/library/Exceptions/SubdivisionCode/DkSubdivisionCodeException.php
@@ -0,0 +1,18 @@
+
+/*
+ *
+ * (c) Alexandre Gomes Gaigalas <alexandre@gaigalas.net>
+ *
+ * For the full copyright and license information, please view the "LICENSE.md"
+ * file that was distributed with this source code.
+ */
+
+namespace Respect\Validation\Exceptions\SubdivisionCode;
+
+use Respect\Validation\Exceptions\SubdivisionCodeException;
+/**
+ * Exception class for Denmark subdivision code.
+ *
+ * ISO 3166-1 alpha-2: DK
+ */
+        self::MODE_NEGATIVE => [
